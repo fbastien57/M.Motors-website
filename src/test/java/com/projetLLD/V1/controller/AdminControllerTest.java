d1782dd9d5c73e03b070d0d7836b2f5a7d9d51ec
franck bastien <franckbastien222@gmail.com>
2026-05-25 21:52:55 +0200

    docker deployment setup

--- src/test/java/com/projetLLD/V1/controller/AdminControllerTest.java
+++ src/test/java/com/projetLLD/V1/controller/AdminControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AdminControllerTest {
 
     @Autowired
